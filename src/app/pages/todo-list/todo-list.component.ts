a0662cb760c395ebcf9f598c407e85bbdd65f19b
AndresL <amlopezr90@gmail.com>
2020-08-09 02:48:12 -0500

    data model header information

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HeaderModel} from '../../models/header.model';
+import {UtilsService} from '../../services/utils/utils.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  headerData: HeaderModel;
 
-  constructor() { }
+  constructor(private utilsService: UtilsService) {
+    this.initializeHeader();
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  initializeHeader() {
+    const currentDate = this.utilsService.getHeaderFormatDate();
+    const avatarUrl = 'https://api.adorable.io/avatars/48/amlopezr90@gmail.com';
+    this.headerData = new HeaderModel('To do list', currentDate, avatarUrl);
+
   }
 
 }
