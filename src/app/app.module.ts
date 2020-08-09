784bba68569c0d248ec7085f6a0bb25e3cb3d28f
AndresL <amlopezr90@gmail.com>
2020-08-09 00:02:05 -0500

    add header, card & todo-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    HeaderComponent,
+    TodoCardComponent
   ],
   imports: [
     BrowserModule,
